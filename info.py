45352243bf9828fed2e4f74baba1a8174a6123fa
Farook13 <umarulfarook787@gmail.com>
2025-06-24 01:17:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ REQ_CHANNEL2 = (int(REQ_CHANNEL2) if REQ_CHANNEL2 and id_pattern.search(REQ_CHAN
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gameropm390:gameropm390@cluster0.9yu7byf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://gameropm390:gameropm390@cluster0.9yu7byf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://Nigga:nigga@cluster0.neynray.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI3 = environ.get('DATABASE_URI3', "mongodb+srv://gameropm390:gameropm390@cluster0.9yu7byf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TELEGRAM_FILES')
